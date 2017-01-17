1560ef30f9711cb9a4283a50c18eaf54b40717d2
ItsJimi <jmaiquez@student.42.fr>
2017-01-17 12:44:07 +0100

    Add rainbow.sh

---
+++ rainbow.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+color1="\033["
+color2=41
+color3="m"
+
+while [ $color2 -lt 48 ]
+do
+	color=$color1$color2$color3
+	echo $color
+	color2=`expr $color2 + 1`
+	if [ $color2 -eq 47 ]
+		then color2=41
+	fi
+done
